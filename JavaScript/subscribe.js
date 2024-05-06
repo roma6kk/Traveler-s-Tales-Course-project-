925020c465e30f321ea2f180e108e59e75bf45ea
roman <romaananyev8@gmail.com>
2024-05-07 00:47:27 +0300

    fwe

--- JavaScript/subscribe.js
+++ JavaScript/subscribe.js
@@ -34,3 +34,40 @@ document.getElementById("send").addEventListener("click", function() {
     URL.revokeObjectURL(jsonUrl);
     URL.revokeObjectURL(xmlUrl);
 });
+
+document.getElementById("send1").addEventListener("click", function() {
+    let email = document.getElementById("email1").value;
+
+    let userData = {
+        email: email
+    };
+
+    let jsonData = JSON.stringify(userData, null, 2);
+
+    let xmlData = '<?xml version="1.0" encoding="UTF-8"?>\n';
+    xmlData += '<user>\n';
+    xmlData += '  <email>' + email + '</email>\n';
+    xmlData += '</user>';
+
+    let jsonBlob = new Blob([jsonData], { type: "application/json" });
+    let jsonUrl = URL.createObjectURL(jsonBlob);
+
+    let xmlBlob = new Blob([xmlData], { type: "application/xml" });
+    let xmlUrl = URL.createObjectURL(xmlBlob);
+
+    let jsonLink = document.createElement("a");
+    jsonLink.href = jsonUrl;
+
+    let xmlLink = document.createElement("a");
+    xmlLink.href = xmlUrl;
+
+    let timestamp = new Date().getTime();
+    jsonLink.download = "data_email_" + timestamp + ".json";
+    xmlLink.download = "data_email_" + timestamp + ".xml";
+
+    jsonLink.click();
+    xmlLink.click();
+
+    URL.revokeObjectURL(jsonUrl);
+    URL.revokeObjectURL(xmlUrl);
+});
